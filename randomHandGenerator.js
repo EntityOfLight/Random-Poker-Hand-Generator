987e67d264f7256bc5ec100c21a6144b881257c1
Samuel Pearse Walters <samuel.p.walters@gmail.com>
2019-02-15 20:20:28 -0600

    added deckLength var to so that Math.random doesn't have to be between 1 & 52

--- randomHandGenerator.js
+++ randomHandGenerator.js
@@ -1,13 +1,14 @@
-let cardDeck = ['Ace of Spades', 'Ace of Clubs', 'Ace of Hearts', 'Ace of Diamonds'];
+let cardDeck = ['Ace of Spades', 'Ace of Clubs', 'Ace of Hearts', 'Ace of Diamonds'],
+    deckLength = cardDeck.length; // because you'll be accessing the length of the deck later
 
 //console.log(cardDeck);
 
-let cardOne = Math.floor(Math.random()*52);
-let cardTwo = Math.floor(Math.random()*52);
+let cardOne = Math.floor(Math.random()*deckLength);
+let cardTwo = Math.floor(Math.random()*deckLength);
 let theCards = (`${cardOne}, ${cardTwo}`);
 while (cardOne === cardTwo) {
        Math.floor(Math.random()*52) = cardTwo;
-       }
+    }
 console.log(theCards);
 
 //Thinking that I can use cardOne & cardTwo values to maybe call those specific values from the array.
